4e01a91aed2b8a94871dfb182892c6bb855d74f4
2oothpick <kobbyboateng14@gmail.com>
2024-11-11 18:01:44 +0000

    Add getHumanChoice function

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ function getComputerChoice() {
     return result == 1 ? 'rock' : result == 2 ? 'paper' : 'scissors';
 }
 
+function getHumanChoice() {
+    /* Prompts and returns human's choice */
+    let result = prompt('Choose between "rock", "paper" and "scissors":');
+    return result.toLowerCase();
+}
+
