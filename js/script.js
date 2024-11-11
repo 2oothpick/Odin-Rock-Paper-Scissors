3eaf3dfb47abc6b59007f79af35e920ce465c08a
2oothpick <kobbyboateng14@gmail.com>
2024-11-11 18:29:45 +0000

    Add function to play a single game

--- js/script.js
+++ js/script.js
@@ -10,3 +10,36 @@ function getHumanChoice() {
     return result.toLowerCase();
 }
 
+/* score */
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    console.log(humanChoice, computerChoice)
+    if (humanChoice == computerChoice) {
+        return 'Draw, nobody wins';
+    } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        computerScore++;
+        return 'You lose! Paper beats Rock.';
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        humanScore++;
+        return 'You Win! Rock beats Scissors.';
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        computerScore++;
+        return 'You Lose! Scissors beats Paper.';
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        humanScore++;
+        return 'You Win! Scissors beats Paper.';
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        computerScore++;
+        return 'You Lose! Rock beats Scissors.';
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore++;
+        return 'You Win! Paper beats Rock.';
+    } else {
+        console.log('Check spelling')
+    }
+}
+
