ec837eb647a372678da57abd0e29eb7a7397b875
2oothpick <kobbyboateng14@gmail.com>
2024-12-16 12:19:49 +0000

    Add info div to display game status

--- js/script.js
+++ js/script.js
@@ -11,74 +11,72 @@ function getComputerChoice() {
 
 let humanChoice = '';
 
-function playGame() {
-  /* Loops playRound for 5 rounds */
-  /* score */
-  console.log('started');
-  const guestScore = document.querySelector('.guest_score');
-  let humanScore = Number(guestScore.textContent);
-  const pcScore = document.querySelector('.pc_score');
-  let computerScore = Number(pcScore.textContent);
+/* score */
 
+const guestScore = document.querySelector('.guest_score');
+const pcScore = document.querySelector('.pc_score');
+const round= document.querySelector('.round')
+
+function playGame() {
+  let humanScore = Number(guestScore.textContent);
+  let computerScore = Number(pcScore.textContent);
+  const info = document.querySelector('.info');
+  info.textContent = 'Started';
+  let counter = 0
   function playRound(humanChoice) {
     /* Runs a single round */
-    //humanChoice = getHumanChoice();
-    console.log(humanChoice);
+    counter++
+    round.textContent = `round: ${counter}`
     let computerChoice = getComputerChoice();
-    console.log(humanChoice, computerChoice);
+    console.log(computerChoice, humanChoice);
     if (humanChoice === computerChoice) {
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nDraw, nobody wins`,
-      );
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'Draw, nobody wins';
     } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-      computerScore++;
-      pcScore.textContent = computerScore;
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nYou lose! Paper beats Rock.`,
-      );
+      computerScore += 1;
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'You lose! Paper beats Rock';
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-      humanScore++;
-      guestScore.textContent = humanScore;
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nYou Win! Rock beats Scissors.`,
-      );
+      humanScore += 1;
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'You Win! Rock beats Scissors';
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-      computerScore++;
-      pcScore.textContent = computerScore;
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nYou Lose! Scissors beats Paper.`,
-      );
+      computerScore += 1;
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'You Lose! Scissors beats Paper';
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-      humanScore++;
-      guestScore.textContent = humanScore;
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nYou Win! Scissors beats Paper.`,
-      );
+      humanScore += 1;
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'You Win! Scissors beats Paper';
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-      computerScore++;
-      pcScore.textContent = computerScore;
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nYou Lose! Rock beats Scissors.`,
-      );
+      computerScore += 1;
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'You Lose! Rock beats Scissors';
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-      humanScore++;
-      guestScore.textContent = humanScore;
-      window.alert(
-        `Your Score: ${humanScore} Computer's Score: ${computerScore}\nYou Win! Paper beats Rock.`,
-      );
-    } 
-    if (humanScore === 5){
-      window.alert('Congratulations! You won the game!')
-    } else if(computerScore === 5){
-      window.alert('Game over! The computer won.')
+      humanScore += 1;
+      pcScore.textContent = `${computerScore}`;
+      guestScore.textContent = `${humanScore}`;
+      info.textContent = 'You Win! Paper beats Rock';
     }
-
+    if (guestScore.textContent === '5') {
+      info.textContent = 'Congratulations! You won the game!';
+    } else if (pcScore.textContent === '5') {
+      info.textContent = 'Game over! The computer won.';
+    }
+    console.log(humanScore, computerScore);
   }
   // human inputs
   // rock
   const rock = document.querySelector('.rock_btn');
   rock.addEventListener('click', () => {
     if (humanScore < 5 && computerScore < 5) {
+      console.log(humanScore, computerScore);
       playRound('rock');
     }
   });
@@ -87,6 +85,7 @@ function playGame() {
   const paper = document.querySelector('.paper_btn');
   paper.addEventListener('click', () => {
     if (humanScore < 5 && computerScore < 5) {
+      console.log(humanScore, computerScore);
       playRound('paper');
     }
   });
@@ -95,6 +94,7 @@ function playGame() {
   const scs = document.querySelector('.scissors_btn');
   scs.addEventListener('click', () => {
     if (humanScore < 5 && computerScore < 5) {
+      console.log(humanScore, computerScore);
       playRound('scissors');
     }
   });
@@ -106,5 +106,6 @@ const start_btn = document.querySelector('button.start');
 start_btn.addEventListener('click', () => {
   document.querySelector('.guest_score').textContent = '0';
   document.querySelector('.pc_score').textContent = '0';
+  document.querySelector('.round').textContent = 'round: 0'
   playGame();
 });
